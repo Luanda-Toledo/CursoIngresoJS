b253d05e7ec04d2e8fe2e93cedeba8a48d6c560e
lua <toledoluanda@gmail.com>
2023-01-31 12:02:19 -0300

    ejercicios 1-2-3

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,14 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	//Declaro una variable 
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+
+	//Le asignamos el valor ingresado por el usuario a la variable
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+
+	//Pasamos el valor de la variable atraves del alert
+	alert("Bienvenido " + nombreIngresado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,13 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//Declaro una variable 
+	var nombreIngresado;
+
+	//Le asignamos el valor ingresado por el usuario a la variable
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+
+	//Pasamos el valor de la variable atraves del alert
+	alert("Bienvenido " + nombreIngresado);
 }
 
